28bed6ecdda19add034b67c2b76e32c338e3d546
vladimir <slr-klesks-300@yandex.ru>
2022-09-17 08:17:16 +1000

    Username

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вова")
+{
+    System.Console.WriteLine("Приветству, Владимир!!!");
+}
+else
+{
+System.Console.WriteLine("Привет, ");
+System.Console.WriteLine(username);
+}
